7e42032f6baf633be189e65314f7fdcd707c13ea
hamadash <60833258+hamadash@users.noreply.github.com>
2024-03-31 17:01:09 +0900

    add: tokyonightを追加

--- .config/nvim/lua/plugins/scrollbar.lua
+++ .config/nvim/lua/plugins/scrollbar.lua
@@ -3,4 +3,20 @@ return {
   dependencies = {
     "kevinhwang91/nvim-hlslens",
   },
+  config = function ()
+    local colors = require("tokyonight.colors").setup()
+    require("scrollbar").setup({
+      handle = {
+        color = colors.bg_highlight,
+      },
+      marks = {
+        Search = { color = colors.orange },
+        Error = { color = colors.error },
+        Warn = { color = colors.warning },
+        Info = { color = colors.info },
+        Hint = { color = colors.hint },
+        Misc = { color = colors.purple },
+      }
+    })
+  end
 }
