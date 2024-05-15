f05f564e4102f47fc8368f3cac430891fc244554
hamadash <60833258+hamadash@users.noreply.github.com>
2024-05-15 09:11:57 +0900

    add: coc-nvimで管理するLSPを定義

--- .config/nvim/lua/plugins/coc-nvim.lua
+++ .config/nvim/lua/plugins/coc-nvim.lua
@@ -2,6 +2,21 @@ return {
   "neoclide/coc.nvim",
   branch = "release",
   event = "InsertEnter",
+  init = function()
+    vim.g.coc_global_extensions = {
+      "coc-eslint",
+      "coc-git",
+      "coc-json",
+      "coc-lua",
+      "coc-markdownlint",
+      "coc-prettier",
+      "coc-solargraph",
+      "coc-sql",
+      "coc-tslint-plugin",
+      "coc-tsserver",
+      "coc-typos",
+    }
+  end,
   keys = {
     { "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><C-r>=coc#on_enter()<CR>"]], mode = "i", expr = true, replace_keycodes = false },
     { "<Tab>", [[coc#pum#visible() ? coc#pum#next(1) : "<Tab>"]], mode = "i", expr = true, replace_keycodes = false },
