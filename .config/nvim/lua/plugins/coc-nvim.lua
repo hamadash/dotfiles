afbc83c9162ee8e7e9e0179129aecb5c4f8291c7
hamadash <60833258+hamadash@users.noreply.github.com>
2024-05-30 21:27:58 +0900

    comment: Enterキーで確定できないときがある旨のコメント

--- .config/nvim/lua/plugins/coc-nvim.lua
+++ .config/nvim/lua/plugins/coc-nvim.lua
@@ -18,6 +18,7 @@ return {
     }
   end,
   keys = {
+    -- Enterキーの動作が不安定だが、原因がわからない。
     { "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><C-r>=coc#on_enter()<CR>"]], mode = "i", expr = true, replace_keycodes = false },
     { "<Tab>", [[coc#pum#visible() ? coc#pum#next(1) : "<Tab>"]], mode = "i", expr = true, replace_keycodes = false },
     { "<S-Tab>", [[coc#pum#visible() ? coc#pum#prev(1) : "<S-Tab>"]], mode = "i", expr = true, replace_keycodes = false },
