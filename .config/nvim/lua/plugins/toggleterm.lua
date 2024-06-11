a37e72697a16df879ce410d13fb070f681897cc1
hamadash <60833258+hamadash@users.noreply.github.com>
2024-06-12 08:34:31 +0900

    change: toggletermをfloatで開く

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,7 @@ return {
   "akinsho/toggleterm.nvim",
   opts = {
     size = 20,
+    direction = "float",
   },
   keys = {
     { "<C-t>", [["<CMD>ToggleTerm<CR>"]], mode = "n", expr = true, replace_keycodes = false },
