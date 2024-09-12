6364d6aaec12e5664b00174556cf6a4271781b2b
hamadash <60833258+hamadash@users.noreply.github.com>
2024-09-12 21:27:24 +0900

    remove: lintエラーが出るので削除

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,12 +15,6 @@ return {
 
 		require("mason").setup()
 		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"htmlhint",
-				"jsonlint",
-				"markdownlint",
-				"yamllint",
-			},
 			automatic_installation = true,
 		})
 
