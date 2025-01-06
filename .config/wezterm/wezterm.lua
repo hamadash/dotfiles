9128fd7c79f0c18ae2878b3d13a58cf15ebce2bc
hamadash <60833258+hamadash@users.noreply.github.com>
2025-01-06 09:01:35 +0900

    update: 設定を少し変えてみた

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,11 +6,13 @@ local config = wezterm.config_builder()
 
 config.font_size = 14.0
 config.window_background_opacity = 0.80
+config.window_decorations = "RESIZE"
 
--- かっこいいけど迷う
--- config.window_decorations = "RESIZE"
--- config.macos_window_background_blur = 10
+-- 起動時のウィンドウサイズ TODO: window:maximize() が使える？
+config.initial_rows = 50
+config.initial_cols = 180
 
+-- keybinds
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
 config.disable_default_key_bindings = true
