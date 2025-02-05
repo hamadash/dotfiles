2ad87d4d732802f8083fd23ec77bd188aa5e0d0e
hamadash <60833258+hamadash@users.noreply.github.com>
2025-02-05 12:59:19 +0900

    add: aerialをVSCodeでも使えるようにする

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 if vim.g.vscode then
 	require("lazy").setup({
 		spec = {
+			{ import = "plugins.aerial" },
 			{ import = "plugins.any-jump" },
 			{ import = "plugins.clever-f" },
 			{ import = "plugins.colorizer" },
