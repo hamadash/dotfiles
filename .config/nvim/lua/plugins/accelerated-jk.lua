e014f0246eef3abb75400b9c65cdb3df20594fea
hamadash <60833258+hamadash@users.noreply.github.com>
2025-03-15 11:17:26 +0900

    add: accelerated-jkを追加

---
+++ .config/nvim/lua/plugins/accelerated-jk.lua
@@ -0,0 +1,7 @@
+return {
+	"rainbowhxch/accelerated-jk.nvim",
+	keys = {
+		{ mode = "n", "j", "<Plug>(accelerated_jk_gj)", desc = "accelerated gj movement" },
+		{ mode = "n", "k", "<Plug>(accelerated_jk_gk)", desc = "accelerated gk movement" },
+	},
+}
