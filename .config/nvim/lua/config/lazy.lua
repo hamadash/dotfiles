7cb4ba265e7a17b6e2ed625e79b5466017ef6a76
hamadash <60833258+hamadash@users.noreply.github.com>
2025-03-18 08:51:47 +0900

    comment: accelerated-jkがVSCodeで効いていないかもしれないのでTODOコメントだけ追加

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 if vim.g.vscode then
 	require("lazy").setup({
 		spec = {
-			{ import = "plugins.accelerated-jk" },
+			{ import = "plugins.accelerated-jk" }, -- TODO: VSCode で効いてないかも
 			{ import = "plugins.aerial" },
 			{ import = "plugins.any-jump" },
 			{ import = "plugins.clever-f" },
