bc0a9df0fbc8ea6b906aed1ea3456643bd7a7ba8
hamadash <60833258+hamadash@users.noreply.github.com>
2025-03-21 09:03:14 +0900

    update: デフォルトで折りたたまれないように

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -2,9 +2,10 @@ return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = { "kevinhwang91/promise-async" },
 	config = function()
-		require("ufo").setup()
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
 
-		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		require("ufo").setup()
 	end,
 }
