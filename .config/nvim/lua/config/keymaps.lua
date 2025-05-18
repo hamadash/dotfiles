566120a385abe310de531a00a429b0066676c3da
hamadash <60833258+hamadash@users.noreply.github.com>
2025-05-19 08:52:05 +0900

    change: q をプレフィックスキーにする (ref. https://zenn.dev/vim_jp/articles/43d021f461f3a4#q%E3%82%92%E3%83%97%E3%83%AC%E3%83%95%E3%82%A3%E3%83%83%E3%82%AF%E3%82%B9%E3%81%AB%E3%81%99%E3%82%8B)

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -82,6 +82,16 @@ set_keymap("n", "<Leader>ro", "<C-^>", { desc = "Reopen recent closed buffer." }
 -- Cursor で開く
 set_keymap("n", "<Leader>oc", "<CMD>Cursor<CR>", { desc = "Open current file in Cursor." })
 
+-- q をプレフィックスキーにする
+-- see: https://zenn.dev/vim_jp/articles/43d021f461f3a4#q%E3%82%92%E3%83%97%E3%83%AC%E3%83%95%E3%82%A3%E3%83%83%E3%82%AF%E3%82%B9%E3%81%AB%E3%81%99%E3%82%8B
+set_keymap(
+	"n",
+	"<script><expr> q",
+	"empty(reg_recording()) ? '<sid>(q)' : 'q'",
+	{ desc = "Use as prefix unless recording macro." }
+)
+set_keymap("n", "<sid>(q)q", "qq", { desc = "Start macro recording when not using q as prefix." })
+
 -- LazyVim setteings start --
 -- LazyVim のキーマップが便利なので流用
 -- https://www.lazyvim.org/configuration/general#keymaps
