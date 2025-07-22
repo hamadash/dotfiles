18ba732448a42d8a672cb16206a3d1cc1435c7f5
hamadash <60833258+hamadash@users.noreply.github.com>
2025-07-22 09:32:59 +0900

    remove: コメントを削除

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -5,8 +5,6 @@ return {
 	opts = {},
   -- stylua: ignore
   keys = {
-    -- r の便利な利用方法
-    -- https://minerva.mamansoft.net/2024-03-27-underrated-essential-neovim-plugins#:~:text=%E7%B4%B9%E4%BB%8B%E3%81%97%E3%81%BE%E3%81%99%E3%80%82-,Remote%20Actions,-%E7%8F%BE%E5%9C%A8%E3%81%AE%E3%82%AB%E3%83%BC%E3%82%BD%E3%83%AB
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "<Leader>hh", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
   },
