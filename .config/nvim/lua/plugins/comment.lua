9e4ff03cc54fc7948df7cec05e5058ce65701c69
hamadash <60833258+hamadash@users.noreply.github.com>
2026-01-31 14:52:27 +0900

    update: /が効かないことがあるので二重に設定

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,11 @@
 return {
-  "numToStr/Comment.nvim",
-  keys = {
-    { "<C-_>", "<Plug>(comment_toggle_linewise_current)", mode = "n" },
-    { "<C-_>", "<Plug>(comment_toggle_linewise_visual)",  mode = "v" },
-  },
-  lazy = false,
+	"numToStr/Comment.nvim",
+	keys = {
+		{ "<C-_>", "<Plug>(comment_toggle_linewise_current)", mode = "n" },
+		{ "<C-_>", "<Plug>(comment_toggle_linewise_visual)", mode = "v" },
+		-- _ だと / でコメントアウトできないことがあったので一応定義
+		{ "<C-/>", "<Plug>(comment_toggle_linewise_current)", mode = "n" },
+		{ "<C-/>", "<Plug>(comment_toggle_linewise_visual)", mode = "v" },
+	},
+	lazy = false,
 }
