a9d016da67ee64588091495adafd982fc9ed611a
hamadash <60833258+hamadash@users.noreply.github.com>
2026-02-10 09:01:12 +0900

    change: Acceptがtabだと補完とコンフリするので<C-L>に割り当ててみる

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,4 +1,8 @@
 return {
 	"github/copilot.vim",
 	lazy = false,
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.api.nvim_set_keymap("i", "<C-L>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+	end,
 }
