a872699af3eaac7c738ef9fb474ca6dfb58b6029
hamadash <60833258+hamadash@users.noreply.github.com>
2026-02-15 13:26:17 +0900

    change: Accept時のキーをkに変更

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,6 +3,6 @@ return {
 	lazy = false,
 	config = function()
 		vim.g.copilot_no_tab_map = true
-		vim.api.nvim_set_keymap("i", "<C-L>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+		vim.api.nvim_set_keymap("i", "<C-k>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
 	end,
 }
