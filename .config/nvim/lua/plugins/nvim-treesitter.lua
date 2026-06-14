d7c917a8060b92048eb74320a1a45e2a1a926a49
hamadash <60833258+hamadash@users.noreply.github.com>
2026-06-15 08:52:38 +0900

    style: フォーマット

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,19 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = "UIEnter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "RRethy/nvim-treesitter-endwise",
-    "windwp/nvim-ts-autotag"
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      auto_install = false,
-      ensure_installed = "all",
-      endwise = { enable = true },
-      autotag = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "UIEnter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"RRethy/nvim-treesitter-endwise",
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = { enable = true },
+			auto_install = false,
+			ensure_installed = "all",
+			endwise = { enable = true },
+			autotag = { enable = true },
+		})
+	end,
 }
